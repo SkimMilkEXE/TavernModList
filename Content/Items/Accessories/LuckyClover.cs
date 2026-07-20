a75d57de2450f9bf5b0dea29b8bf1e10333e7080
Richard Hurley <richardhurley374@gmail.com>
2026-07-19 21:58:31 -0400

    Temp Removed LuckyClover

--- Content/Items/Accessories/LuckyClover.cs
+++ Content/Items/Accessories/LuckyClover.cs
@@ -1,23 +1,23 @@
-using Terraria;
-using Terraria.ID;
-using Terraria.ModLoader;
+// using Terraria;
+// using Terraria.ID;
+// using Terraria.ModLoader;
 
-namespace TavernModList.Content.Items.Accessories
-{
-	public class LuckyClover : ModItem
-	{
-		public override void SetDefaults()
-		{
-			Item.width = 20;
-			Item.height = 20;
-			Item.accessory = true;
-			Item.value = Item.sellPrice(gold: 5);
-			Item.rare = ItemRarityID.LightRed;
-		}
+// namespace TavernModList.Content.Items.Accessories
+// {
+// 	public class LuckyClover : ModItem
+// 	{
+// 		public override void SetDefaults()
+// 		{
+// 			Item.width = 20;
+// 			Item.height = 20;
+// 			Item.accessory = true;
+// 			Item.value = Item.sellPrice(gold: 5);
+// 			Item.rare = ItemRarityID.LightRed;
+// 		}
 
-		public override void UpdateAccessory(Player player, bool hideVisual)
-		{
-			player.fishingSkill += 15;
-		}
-	}
-}
+// 		public override void UpdateAccessory(Player player, bool hideVisual)
+// 		{
+// 			player.fishingSkill += 15;
+// 		}
+// 	}
+// }
